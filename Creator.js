9a6ebf377b5deb5df346c20317eb29c1b27d9f76
Corazon <41511738+assafgold1@users.noreply.github.com>
2023-11-24 22:43:44 +0200

    Create Creator.js

---
+++ Creator.js
@@ -0,0 +1,109 @@
+const sequence = [];
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("A" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("B" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("C" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("D" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("H" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("J" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("I" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("E" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("F" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("K" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("L" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("M" + i);
+}
+
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("N" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("O" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("P" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("Q" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("R" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("S" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("T" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("U" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("V" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("W" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("X" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("Y" + i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    sequence.push("Z" + i);
+}
+
+const data = {
+    sequence: sequence
+};
+
+const jsonString = JSON.stringify(data, null, 2);
+console.log(jsonString);
